07bb2e9822c61799a96d378a7f41287a1d8f3a03
David Hagerman Olzon <david.hagerman.olzon@gmail.com>
2019-05-08 17:04:35 +0200

    last update for the lab

--- Assignment 3/mp-pi-montecarlo-pool.py
+++ Assignment 3/mp-pi-montecarlo-pool.py
@@ -2,18 +2,20 @@
 import multiprocessing as mp# See https://docs.python.org/3/library/multiprocessing.html
 import argparse # See https://docs.python.org/3/library/argparse.html
 import random
-import queue
+import time
 from math import pi
 
-def sample_pi(result_queue):
+def sample_pi(result_queue, seed):
+    # Using a static start seed to be able to compare different runs
+    random.seed(seed)
     while (True):
-        random.seed(10)
         s = 0
         x = random.random()
         y = random.random()
         if x**2 + y**2 <= 1.0:
             s += 1
-        result_queue.put(s)
+        result_queue.put(s)    
+        # Seed needs to change or accuracy will also stay static
     
 
 def estimator(result_queue, workers, acc_target):
@@ -22,6 +24,7 @@ def estimator(result_queue, workers, acc_target):
     accuracy = 0
     while (accuracy < acc_target):
         successes = result_queue.get()
+
         n_total += 1
         s_total = s_total + successes
         pi_est = (4.0*s_total)/n_total
@@ -35,11 +38,15 @@ def compute_pi(args):
 
     jobs = []
 
+    
     for i in range(args.workers):
-        p = mp.Process(target=sample_pi, args=(result_queue,))
+        p = mp.Process(target=sample_pi, args=(result_queue,i))
         jobs.append(p)
         p.start()
+    start = time.time()
     n_total, s_total, pi_est, error, accuracy = estimator(result_queue, args.workers, args.accuracy)
+    measured_time = time.time() - start
+    print(measured_time)
 
     for i in range(args.workers):
         jobs[i].terminate()
