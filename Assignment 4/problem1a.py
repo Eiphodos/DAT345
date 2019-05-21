171fec8fa5839c926cd8d66cc66305f8bdfd147d
Eiphodos <david.hagerman.olzon@gmail.com>
2019-05-21 14:10:16 +0200

    New version with an additional map/combine/reduce step

--- Assignment 4/problem1a.py
+++ Assignment 4/problem1a.py
@@ -3,6 +3,7 @@ import sys
 import time
 import os
 import tempfile
+import math
 
 class Problem1a(MRJob):
 
@@ -11,13 +12,15 @@ class Problem1a(MRJob):
     
     def mean_dev(self, value, mean):
         return abs(value - mean)
-    '''
+    
     def steps(self):      
         return [MRStep( mapper=self.mapper,
                         combiner=self.combiner,
                         reducer=self.reducer),
-                        MRStep(reducer=self.result)]
-    ''' 
+                        MRStep(mapper=self.mean_dev_mapper,
+                                combiner=self.mean_dev_combiner,
+                                reducer=self.mean_dev_reducer)]
+    
 
     def mapper(self, _, line):
         splitline = line.split()
@@ -29,13 +32,11 @@ class Problem1a(MRJob):
         minimum = sys.float_info.max
         maximum = -sys.float_info.max
         partial_sum = 0
-        partial_sum_squared = 0
         bins = [0] * 10
         values = []
         for i, c in enumerate(counts):
             values.append(c)
             partial_sum += c
-            partial_sum_squared += c ** 2
             if c < minimum:
                 minimum = c
             if c > maximum:
@@ -43,7 +44,7 @@ class Problem1a(MRJob):
             bindex = int(c)
             bins[bindex] += 1
         nr_lines = i +1
-        yield ("stats", (nr_lines, partial_sum, partial_sum_squared, values))
+        yield ("stats", (nr_lines, partial_sum, values))
         yield ("bins", bins)
         yield ("minimum", minimum)
         yield ("maximum", maximum)
@@ -53,21 +54,16 @@ class Problem1a(MRJob):
         if key == "stats":
             total_lines = 0
             total_sum = 0
-            total_sum_squared = 0
-            final_max = 0
             all_values = []
             for c in counts:
                 total_lines += c[0]
                 total_sum += c[1]
-                total_sum_squared += c[2]
-                all_values += c[3]
+                all_values += c[2]
 
             mean = float(total_sum)/total_lines
-            stand_dev = (total_sum_squared - (total_sum * total_sum)/total_lines)/(total_lines-1)
-            yield("Standard deviation: ", stand_dev)
 
-            mean_deviation = sum([self.mean_dev(value, mean) for value in all_values]) / len(all_values)
-            yield("Mean deviation: ", mean_deviation)
+            for v in all_values:
+                yield("deviation", (mean, v))
             
 
         if key == "bins":
@@ -75,8 +71,74 @@ class Problem1a(MRJob):
             for c in counts:
                 for i, b in enumerate(c):
                     final_bins[i] += b
+            yield ("bins", (final_bins[0], final_bins[1], final_bins[2], final_bins[3], final_bins[4],
+                             final_bins[5], final_bins[6], final_bins[7], final_bins[8], final_bins[9]))
+        
+        if key == "minimum":
+            final_min = sys.float_info.max
+            for c in counts:
+                if c < final_min:
+                    final_min = c
+            yield ("min", final_min)
+        
+        if key == "maximum":
+            final_max = -sys.float_info.max
+            for c in counts:           
+                if c > final_max:
+                    final_max = c
+            yield ("max", final_max)
 
-            yield ("X < 1\t", final_bins[0])
+    def mean_dev_mapper(self, key, line):
+        if key == "deviation":
+            mean, value = line
+            deviation = abs(value - mean)
+            std_deviation = (value - mean) ** 2
+            yield("deviation", (deviation, std_deviation))
+        if key == "stddev":
+            yield(key, line)
+        if key == "bins":
+            yield(key, line)
+        if key == "max":
+            yield(key, line)
+        if key == "min":
+            yield(key, line)
+
+    def mean_dev_combiner(self, key, counts):
+        if key == "deviation":
+            partial_sum_mean = 0
+            partial_sum_std = 0
+            for i, c in enumerate(counts):
+                partial_sum_mean += c[0]
+                partial_sum_std += c[1]
+            nr_lines = i +1
+            yield ("dev_stats", (nr_lines, partial_sum_mean, partial_sum_std))
+        if key == "stddev":
+            yield(key, counts)
+        if key == "bins":
+            yield(key, counts)
+        if key == "max":
+            yield(key, counts)
+        if key == "min":
+            yield(key, counts)
+
+    def mean_dev_reducer(self, key, counts):
+        if key == "dev_stats":
+            total_lines = 0
+            total_sum_mean = 0
+            total_sum_std = 0
+            for c in counts:
+                total_lines += c[0]
+                total_sum_mean += c[1]
+                total_sum_std += c[2]
+
+            mean_dev = float(total_sum_mean)/total_lines
+            std_dev = float(total_sum_std)/total_lines
+            yield("Mean deviation: ", mean_dev)
+            yield("Standard deviation: ", std_dev)
+
+        if key == "bins":
+            final_bins = next(counts)[0]
+            yield ("X < 1", final_bins[1])
             yield ("1 <= X < 2", final_bins[1])
             yield ("2 <= X < 3", final_bins[2])
             yield ("3 <= X < 4", final_bins[3])
@@ -86,20 +148,12 @@ class Problem1a(MRJob):
             yield ("7 <= X < 8", final_bins[7])
             yield ("8 <= X < 9", final_bins[8])
             yield ("9 <= X", final_bins[9])
-        
-        if key == "minimum":
-            final_min = sys.float_info.max
-            for c in counts:
-                if c < final_min:
-                    final_min = c
+        if key == "max":
+            final_min = next(counts)[0]
             yield ("Minimum: ", final_min)
-        
-        if key == "maximum":
-            final_max = -sys.float_info.max
-            for c in counts:           
-                if c > final_max:
-                    final_max = c
+        if key == "min":
+            final_max = next(counts)[0]
             yield ("Maximum: ", final_max)
-            
+        
 if __name__ == '__main__':
     Problem1a.run()
